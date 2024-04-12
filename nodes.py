adcbec155d4c04aa692cd37b21b1deb8a20a9c0e
WilliamStanford <54320229+WilliamStanford@users.noreply.github.com>
2024-04-12 17:50:07 -0400

    Create nodes.py

---
+++ nodes.py
@@ -0,0 +1,29 @@
+
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+
+
+def register_node(identifier: str, display_name: str):
+    def decorator(cls):
+        NODE_CLASS_MAPPINGS[identifier] = cls
+        NODE_DISPLAY_NAME_MAPPINGS[identifier] = display_name
+
+        return cls
+
+    return decorator
+
+@register_node("FloatArrayToPointString", "Point String from float array")
+class _:
+    CATEGORY = "visuallabs"
+    INPUT_TYPES = lambda: {
+        "required": {
+            "required": { "float_array" : ("STRING", {}) },
+        }
+    }
+    RETURN_TYPES = ("STRING_LIST",)
+    RETURN_NAMES = ("point_string",)
+    FUNCTION = "format_float_array"
+
+
+    def format_float_array(float_array):
+      return ", ".join(f"{i}: ({x:.2f})" for i, x in enumerate(float_array))
