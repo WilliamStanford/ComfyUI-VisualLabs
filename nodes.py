1697953e929dafe0b3e95cefff00459572c8abe4
WilliamStanford <54320229+WilliamStanford@users.noreply.github.com>
2024-04-16 14:32:31 -0400

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -118,7 +118,7 @@ and interpolating from that to fully black at the 16th frame.
         
         # Parse the input string into a list of tuples
         points = []
-        points_string = points_string.rstrip(',\n')
+        points_string = points_string.rstrip('\n')
         for point_str in points_string.split(','):
             frame_str, color_str = point_str.split(':')
             frame = int(frame_str.strip())
